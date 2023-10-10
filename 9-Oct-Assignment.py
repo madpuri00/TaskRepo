13f62e35df348a34e8259ad0fa6fd1a1a2b2898b
madpuri00 <madpuri_john@yahoo.com>
2023-10-10 16:00:15 -0400

    First Commit for task

---
+++ 9-Oct-Assignment.py
@@ -0,0 +1,21 @@
+my_set = set()
+while len(my_set) != 5:
+    my_set.add(int(input("Enter a number:")))
+    print(my_set)
+
+#  Create a program ,take 5 numbers from the user
+#  add 1-2 duplicates and then print the non-duplicate numbers
+
+print('Please enter any five numbers\n')
+n1 = int(input('Enter first number\n'))
+n2 = int(input('Enter second number\n'))
+n3 = int(input('Enter third number\n'))
+n4 = int(input('Enter fourth number\n'))
+n5 = int(input('Enter fifth number\n'))
+
+set1 = {n1, n2, n3, n4, n5}
+print(set1)
+
+# pwd - to see path
+# cd into TaskRepo
+# git add . - to add everything into folder or git add 9-Oct-Assignment.py
